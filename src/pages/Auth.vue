143e4b2ebb0ab47711ca1960e077d5448c20d2ac
Cristina Palomeque <cristina.palomeque@gmail.com>
2022-12-22 19:42:37 +0100

    Routes for Home + Commit

---
+++ src/pages/Auth.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Login or Register page</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Home from "../pages/Home.vue";
+import Auth from "../pages/Auth.vue";
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
 
-const routes = [];
+  {
+    path: "/auth",
+    name: "Auth",
+    component: Auth,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.VITE_APP_ENV),
