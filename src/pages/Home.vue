796de391954d25cf398c8f65a1d4045fb24ce8f1
Cristina Palomeque <cristina.palomeque@gmail.com>
2022-12-30 12:52:14 +0100

    logout button and path

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,33 @@
 <template>
-  <div>This is the homepage.</div>
+  <h1>Welcome to your To-Do List</h1>
+  <button
+    @click="logout()"
+    class="bg-sky-500 rounded hover:bg-sky-700 px-4 py-2 w-24 m-4"
+  >
+    Log out
+  </button>
 </template>
 
-<script setup></script>
+<script setup>
+import { useUserStore } from "../store/user";
+import { useRouter } from "vue-router";
+
+const userStore = useUserStore();
+const router = useRouter();
+
+function logout() {
+  userStore
+    .logout()
+    .then(() => {
+      // Redirect to Home
+      console.log("You are logged out");
+      router.push({ path: "/auth" });
+    })
+    .catch((e) => {
+      // show error
+      console.log(e);
+    });
+}
+</script>
 
 <style lang="scss" scoped></style>
